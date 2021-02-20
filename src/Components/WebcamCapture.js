52a2c776122a8b188d8b6942dffb66052c2b503e
RyanRau <ryanzrau@gmail.com>
2021-02-20 13:51:23 -0600

    disabled timer for testing

--- src/Components/WebcamCapture.js
+++ src/Components/WebcamCapture.js
@@ -2,14 +2,6 @@ import React from "react";
 import {useEffect, useState} from "react"
 import Webcam from "react-webcam";
 
-// import { Link } from 'react-router-dom';
-
-// import React from "react";
-// import Webcam from "react-webcam";
-
-// const WebcamComponent = () => <Webcam />;
-// export default WebcamComponent;
-
 const videoConstraints = {
   width: 1280,
   height: 720,
@@ -26,58 +18,84 @@ var WebcamCapture = () => {
   const [imageSrc, setImageSrc]=useState("")
   const webcamRef = React.useRef(null);
 
+  function capture(e) {
+    console.log("Picture taken");
+    var currentTime = dateFormat(now, "yyyy-mm-dd h:MM:ss");
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        userId: "af027@uark.edu",
+        lectureId: "1",
+        ts:`${currentTime}`,
+        base64String: webcamRef.current.getScreenshot()
+      }),
+    };
 
+    // KEEP FOR SWITCHING BETWEEN LIVE AND LOCAL BACKEND URL
+    const LIVE_URL = process.env.REACT_APP_BACKEND_URL;
+    let BACKEND_URL = (LIVE_URL) ? LIVE_URL : 'http://0.0.0.0:8080/';
 
+    fetch(BACKEND_URL + 'AWS/image-from-extension', requestOptions)
+      .then((res) => {
+        console.log("res.status: ", res.status);
+        if (res.status === 200) {
+          var response = res.text();
+          console.log("RESPONSE", response);
+        }
+        if (res.status === 500) {
+          console.log("error ");
+        }
+      })
+      .catch((error) => console.log("HELLOOO", error));
+  }
 
-    useEffect(() => {
-     
-      function sendImage() {
-      
-        setInterval(() =>  setImageSrc(webcamRef.current.getScreenshot()), 5000)
-     
-        var currentTime = dateFormat(now, "yyyy-mm-dd h:MM:ss");
-        const requestOptions = {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            userId: "af027@uark.edu",
-            lectureId: "1",
-            ts:`${currentTime}`,
-            base64String:`${imageSrc}`
-          }),
-        };
-    
-        // KEEP FOR SWITCHING BETWEEN LIVE AND LOCAL BACKEND URL
-        const LIVE_URL = process.env.REACT_APP_BACKEND_URL;
-        let BACKEND_URL = (LIVE_URL) ? LIVE_URL : 'http://0.0.0.0:8080/';
-
-        fetch(BACKEND_URL + 'AWS/image-from-extension', requestOptions)
-          .then((res) => {
-            console.log("res.status: ", res.status);
-            if (res.status === 200) {
-              var response = res.text();
-              console.log("RESPONSE", response);
-            }
-            if (res.status === 500) {
-              console.log("error ");
-            }
-          })
-          .catch((error) => console.log("HELLOOO", error));
-    
-    
-      }
-      sendImage();
-  }, [imageSrc])
-
-
+  // useEffect(() => {
   
+  //   function sendImage() {
+    
+  //     setInterval(() =>  setImageSrc(webcamRef.current.getScreenshot()), 5000)
+    
+  //     var currentTime = dateFormat(now, "yyyy-mm-dd h:MM:ss");
+  //     const requestOptions = {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({
+  //         userId: "af027@uark.edu",
+  //         lectureId: "1",
+  //         ts:`${currentTime}`,
+  //         base64String:`${imageSrc}`
+  //       }),
+  //     };
+  
+  //     // KEEP FOR SWITCHING BETWEEN LIVE AND LOCAL BACKEND URL
+  //     const LIVE_URL = process.env.REACT_APP_BACKEND_URL;
+  //     let BACKEND_URL = (LIVE_URL) ? LIVE_URL : 'http://0.0.0.0:8080/';
+
+  //     fetch(BACKEND_URL + 'AWS/image-from-extension', requestOptions)
+  //       .then((res) => {
+  //         console.log("res.status: ", res.status);
+  //         if (res.status === 200) {
+  //           var response = res.text();
+  //           console.log("RESPONSE", response);
+  //         }
+  //         if (res.status === 500) {
+  //           console.log("error ");
+  //         }
+  //       })
+  //       .catch((error) => console.log("HELLOOO", error));
+  
+  //   }
+
+  //   sendImage();
+  // }, [imageSrc])
 
  // setInterval(capture(), 200000)
 
   return (
     <div>
       <Webcam
-      style={{position:"absolute", padding:"500px", marginTop:"-200px"}}
+      // style={{position:"absolute", padding:"500px", marginTop:"-200px"}}
         audio={false}
         height={500}
         ref={webcamRef}
@@ -85,7 +103,7 @@ var WebcamCapture = () => {
         width={500}
         videoConstraints={videoConstraints}
       />
-      {/* <button onClick={capture}>Capture photo</button> */}
+      <button onClick={capture}>Capture photo</button>
 
       {/* {imageSrc && (
         <img
