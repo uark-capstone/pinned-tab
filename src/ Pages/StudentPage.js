10802f8a128aa4095f0357a1ca244b5cafa24cdd
afrye1997 <allisonfrye97@gmail.com>
2021-03-02 06:17:55 -0600

    pages

---
+++ src/ Pages/StudentPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {useEffect, useState} from "react"
+
+
+const StudentPage=()=>{
+    return (
+        <div>
+            StudentPage
+        </div>
+    )
+}
+
+export default StudentPage; 
---
+++ src/ Pages/TeacherPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {useEffect, useState} from "react"
+
+
+const TeacherPage=()=>{
+    return (
+        <div>
+            TeacherPage
+        </div>
+    )
+}
+
+export default TeacherPage; 
