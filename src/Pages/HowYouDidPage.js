6afdd2b4d10f7d373f3d4ae154108b3c1815ec51
RyanRau <ryanzrau@gmail.com>
2021-04-20 21:32:27 -0500

    charting fixes

--- src/Pages/HowYouDidPage.js
+++ src/Pages/HowYouDidPage.js
@@ -5,252 +5,172 @@ import "../reportingpage.css";
 import { Dropdown } from "react-bootstrap";
 import useEmotionsByLecture from "../Hooks/useEmotionsByLecture";
 import useGetAllLectures from "../Hooks/useGetAllLectures";
+import Chart from 'chart.js'
 
 /**
  * Refer here for the library https://www.npmjs.com/package/react-chartjs-2 and you can see all the props you can
  * pass it!!
  */
 
-const randomComments = (
-  score,
-  possibleComments,
-  comments,
-  seed,
-  chartComment
-) => {
-  //More random comment-picking code. Once again, it's mostly for fun.//
+ const gatherData = (input_data) => {
+  let emotion_values = [
+    'Not listening',
+    'Neutral',
+    'Actively Listening'
+  ]
 
-  if (score["CALM"] > 1) {
-    possibleComments.push(comments[1]);
-    possibleComments.push(comments[2]);
-    possibleComments.push(comments[5]);
+  // sort into dict for each user
+  let data_dict = {}
+  let labels = []
+
+  input_data.forEach(element => {
+    let timestamp = new Date(element.timestamp)
+    timestamp = timestamp.valueOf()
+    // timestamp = timestamp.toLocaleTimeString([], {year: 'numeric', month: 'numeric', day: 'numeric', hour: '2-digit', minute: '2-digit'})
+
+    let value = emotion_values.findIndex(x => x == element.emotions)
+
+    let data_entry = {
+      x: timestamp,
+      y: value
+    }
+
+    if (element.user_id in data_dict){
+      data_dict[element.user_id].data.push(data_entry)
+    }else {
+      data_dict[element.user_id] = {
+        user_id: element.user_id,
+        data: []
+      }
+      data_dict[element.user_id].data.push(data_entry)
+    }
+
+    if (!labels.includes(timestamp)){
+      labels.push(timestamp)
+    }
+
+  });
+
+  let data_sets = []
+
+  let minDate = null;
+  let maxDate = null;
+
+  for(var key in data_dict) {
+    // let localMin = data_dict[key].data.reduce((min, p) => new Date(p.x) < new Date(min) ? p.x : min, new Date(data_dict[key].data.x));
+    // let localMax = data_dict[key].data.reduce((max, p) => p.x > max ? p.x : max, data_dict[key].data.y);
+
+    // localMin = new Date(localMin)
+    // localMax = new Date(localMax)
+
+    // if (minDate == null || localMin < minDate ){
+    //   minDate = localMin
+    // }
+
+    // if (maxDate == null || localMax > maxDate ){
+    //   maxDate = localMax
+    // }
+
+    let r = getRandomIntInclusive(0, 255).toString()
+    let g = getRandomIntInclusive(0, 255).toString()
+    let b = getRandomIntInclusive(0, 255).toString()
+
+    data_sets.push({
+      label: data_dict[key].user_id,
+      fill: false,
+      borderColor: 'rgb(' + r + ',' + g + ',' + b + ')',
+      data: data_dict[key].data
+    })
+
   }
-  if (score["SAD"] > 1) {
-    possibleComments.push(comments[7]);
-  }
-  if (score["HAPPY"] > 1) {
-    possibleComments.push(comments[3], comments[4]);
-  }
-  if (score["DISGUSTED"] > 1) {
-    possibleComments.push(comments[8]);
-  }
-  if (score["ANGRY"] > 1) {
-    possibleComments.push(comments[9]);
+
+  // let labels = generateLabels(minDate, maxDate, 5);
+
+  return {
+    labels: labels,
+    datasets: data_sets
   }
 
-  chartComment = possibleComments[seed % possibleComments.length];
-};
+}
 
-const settingDictionaries = (
-  score,
-  emotions,
-  chartTicks,
-  chartData,
-  points
-) => {
-  var jsonData = emotions.sort((x, y) => (x.timestamp > y.timestamp ? 1 : -1));
+const generateLabels = (start, end, binning) => {
+  console.log(start)
+  console.log(end)
 
-  // for (var i = 0; i < jsonData.length; i++) {
-  //   //parsing time
-  //   var time = new Date(jsonData[i].timestamp).toLocaleTimeString("GMT", {
-  //     hour: "2-digit",
-  //     minute: "2-digit",
-  //   });
+  let labels = []
+  while (start < end) {
+    labels.push(start.toLocaleString())
+    start = new Date(start.getTime() + binning*60000);
+  }
 
-  //   if (time.charAt(0) === "0") {
-  //     time = time.substring(1);
-  //   }
-  //   //keeping track of score
-  //   score[jsonData[i].emotions] += 1;
+  return labels;
+}
 
-  //   if (chartTicks.includes(time) === false) {
-  //     chartTicks.push(time);
-  //   }
-  //   chartData[jsonData[i].emotions][time]
-  //     ? (chartData[jsonData[i].emotions][time] += 1)
-  //     : (chartData[jsonData[i].emotions][time] = 1);
-
-  //   var keys = Object.keys(chartData);
-
-  //   keys.splice(keys.indexOf(jsonData[i].emotions), 1);
-  //   keys.forEach((item) => {
-  //     chartData[item][time]
-  //       ? (chartData[item][time] += 0)
-  //       : (chartData[item][time] = 0);
-  //   });
-  // }
-
-  // for (const [key, value] of Object.entries(chartData)) {
-  //   for (const [innerkey, innervalue] of Object.entries(chartData[key])) {
-  //     points[key].push(value[innerkey]);
-  //   }
-  // }
-};
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+}
 
 const HowYouDidPage = () => {
-  const [lectureID, setLectureID] = useState(105); // defaults to 'lecture begin'
-
-  //VARIABLES
-  var score = {
-    HAPPY: 0,
-    SAD: 0,
-    CONFUSED: 0,
-    DISGUSTED: 0,
-    SURPRISED: 0,
-    CALM: 0,
-    FEAR: 0,
-    ANGRY: 0,
-  };
-
-  var chartData = {
-    HAPPY: {},
-    SAD: {},
-    CONFUSED: {},
-    DISGUSTED: {},
-    SURPRISED: {},
-    CALM: {},
-    FEAR: {},
-    ANGRY: {},
-  };
-
-  var points = {
-    HAPPY: [],
-    SAD: [],
-    CONFUSED: [],
-    DISGUSTED: [],
-    SURPRISED: [],
-    CALM: [],
-    FEAR: [],
-    ANGRY: [],
-  };
-
-  // For timestamp, an ordered pair is assigned.//
-  var dataPoints = {};
-  var total = 0;
-  var laughScore = 0;
-  var surpriseScore = 0;
-  var happyScore = 0;
-  var comments = [
-    "No news is better than good news!",
-    "Quiet day today!",
-    "Great job, as always!",
-    "It looks like they really liked what you had to say!",
-    "You made them laugh a lot today!",
-    "Looks like they really learned a lot!",
-    "Great job, you're really engaging your students!",
-    "Looks like your students were a little sad today.",
-    "Looks like some students were grossed out!",
-    "Looks like a few students actually got scared! Yikes!",
-  ];
-  var chartComment = "";
-  var seed = Math.floor(Math.random() * 100);
-  var possibleComments = [];
-  var chartTicks = [];
-
-  const chartOptions = {
-    title: {
-      display: true,
-      text: chartComment,
-      fontColor: "black",
-      defaultFontFamily: "Roboto",
-      defaultFontSize: 14,
-    },
-    legend: {
-      display: true,
-      labels: {
-        font: {
-          family: "Roboto",
-        },
-      },
-    },
-  };
-
-  const state = {
-    responsive: true,
-    labels: chartTicks,
-    datasets: [
-      {
-        data: points["CALM"],
-        label: "Calm",
-        backgroundColor: "#ff602a",
-        borderColor: "#ff602a",
-        fill: false,
-        fontColor: "black",
-      },
-      {
-        data: points["SURPRISED"],
-        label: "Surprised",
-        backgroundColor: "#2a48ff",
-        borderColor: "#2a48ff",
-        fill: false,
-        fontColor: "black",
-      },
-
-      {
-        data: points["HAPPY"],
-        label: "Happy",
-        backgroundColor: "#d518d5",
-        borderColor: "#d518d5",
-        fill: false,
-        fontColor: "black",
-      },
-
-      {
-        data: points["DISGUSTED"],
-        label: "Disguested",
-        backgroundColor: "purple",
-        borderColor: "purple",
-        fill: false,
-        fontColor: "black",
-      },
-      {
-        data: points["CONFUSED"],
-        label: "Confused",
-        backgroundColor: "grey",
-        borderColor: "grey",
-        fill: false,
-        fontColor: "black",
-      },
-
-      {
-        data: points["FEAR"],
-        label: "Other",
-        backgroundColor: "orange",
-        borderColor: "orange",
-        fill: false,
-        fontColor: "black",
-      },
-      {
-        data: points["SAD"],
-        label: "Sad",
-        backgroundColor: "black",
-        borderColor: "black",
-        fill: false,
-        fontColor: "black",
-      },
-    ],
-  };
+  const [lectureID, setLectureID] = useState(105);
 
   //CALLING HOOKS
   const {
     emotions,
     isErrorAllEmotions,
     isLoadedAllEmotions,
-  } = useEmotionsByLecture(lectureID);
+  } = useEmotionsByLecture(105);
   const {
     lectures,
     isAllLecturesError,
     isAllLecturesLoaded,
   } = useGetAllLectures();
 
-  settingDictionaries(score, emotions, chartTicks, chartData, points);
-  randomComments(score, possibleComments, comments, seed, chartComment);
+  let data = {}
 
   useEffect(() =>{
-    settingDictionaries(score, emotions, chartTicks, chartData, points);
-    randomComments(score, possibleComments, comments, seed, chartComment);
+    let emotion_values = [
+      'Not listening',
+      'Neutral',
+      'Actively Listening'
+    ]
+
+    data = gatherData(emotions)
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: {
+        responsive:true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            ticks: {
+              min: 0,
+              max: 2,
+              stepSize: 1,
+              callback: function(value, index, values) {
+                return emotion_values[value];
+              }
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              min: 0,
+              max: 50,
+              stepSize: 1,
+              callback: function(value, index, values) {
+                let d = new Date(value);
+                return d.toLocaleString()
+              }
+            }
+          }]
+        }
+      }
+    });
+    console.log('data', data)
   }, [emotions])
-  
 
   return (
     <div>
@@ -260,9 +180,7 @@ const HowYouDidPage = () => {
           id="shadow"
           align="center"
         >
-          <div className="chart">
-            <Line width="1" height="1" data={state} options={chartOptions} />
-          </div>
+          <canvas id="myChart" width="1" height="1"></canvas>
           <Dropdown>
             <Dropdown.Toggle variant="success" id="dropdownMenu">
               Session
